ee75669ba1458950d48cb8aa0fcff5aaa9da424f
Kijasss <95722807@qq.com>
2023-02-25 14:23:04 +0800

    feat: 添加简介组件

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Introduction from './views/Introduction.vue'
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside class="aside">
+        <Introduction></Introduction>
+      </el-aside>
+      <el-container>
+        <el-header class="header"></el-header>
+        <el-main class="main">Main</el-main>
+        <!-- <el-footer class="footer"></el-footer> -->
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.common-layout {
+  background-color: #f7f8f9;
+}
+.aside {
+  background-color: white;
+  height: 90vh;
+  width: 300px;
+  margin: 30px;
+  border-radius: 20px;
+}
+.main {
+  background-color: white;
+  height: 90vh;
+  flex: 1;
+  border-radius: 20px;
+  margin: 30px 30px 30px 10px;
+}
+
+.header {
+  background-color: white;
+  border-radius: 20px;
+}
+.footer {
+  background-color: white;
+  border-radius: 20px;
+}
+</style>
---
+++ src/views/Introduction.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="demo-type">
+    <div>
+      <el-avatar
+        src="https://img-blog.csdnimg.cn/5d67199980614a1ab03ebdfadcc99aed.png"
+        :size="100"
+        class="avatar"
+      />
+      <el-icon style="position: absolute; top: 45px"><Star /></el-icon>
+      <el-icon style="position: absolute; top: 90px; left: 80px"><Star /></el-icon>
+      <strong
+        style="display: block; font-size: 18px; padding-bottom: 10px; color: rgb(205, 205, 239)"
+        >3speech</strong
+      >
+      <div v-for="(item, index) in SchoolList" class="school">
+        <!-- <span v-if="index === 0">
+          <el-icon><Promotion /></el-icon>
+        </span>
+        <span v-if="index === 1">
+          <el-icon><MoonNight /></el-icon>
+        </span> -->
+        {{ item }}
+      </div>
+      <div class="tab" style="margin-top: 6px">
+        <el-icon><OfficeBuilding /></el-icon>
+        &nbsp;网易有道
+      </div>
+      <div class="tab" style="margin-bottom: 3px">
+        <el-icon><Location /></el-icon>
+        &nbsp;福建 - 三明
+      </div>
+      <div class="tab">
+        <el-icon><Message /></el-icon>
+        &nbsp;95722807@qq.com
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const SchoolList = ['U专在读', '2020.9 - 2024.6']
+</script>
+
+<style scoped>
+.demo-type {
+}
+.demo-type > div {
+  flex: 1;
+  text-align: center;
+}
+
+.demo-type > div:not(:last-child) {
+  border-right: 1px solid var(--el-border-color);
+}
+
+.avatar {
+  margin: 50px 0 15px 0;
+}
+
+.school {
+  font-size: 14px;
+  padding: 3px;
+  text-align: center;
+}
+
+.tab {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 3px;
+  font-size: 14px;
+}
+
+.icon {
+  display: inline-block;
+}
+</style>
